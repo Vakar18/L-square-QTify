a0e5352abb90a78cccce8f1a1b536149000dd182
vakar <vakarahmad99@gmail.com>
2023-11-05 13:13:56 +0530

    C4: Search Bar

--- qtify/src/Components/SearchBox/searchBar.js
+++ qtify/src/Components/SearchBox/searchBar.js
@@ -1,6 +1,35 @@
+import { ReactComponent as SearchIcon } from "./search-icon.svg";
 
 export default () => {
   return (
-   <h2>SearchBox</h2>
+    <form
+      style={{
+        display: "flex",
+        minWidth: "570px",
+      }}
+    >
+      <input
+        placeholder="Search an album of your choice"
+        style={{
+          flex: 1,
+          borderRadius:"8px 0px 0px 8px",
+          padding:"8px"
+        }}
+      />
+      <button
+        style={{
+          width: "66px",
+          height: "48px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius:"0px 8px 8px 0px",
+          padding:"8px",
+          cursor:"pointer"
+        }}
+      >
+        <SearchIcon />
+      </button>
+    </form>
   );
-}
+};
